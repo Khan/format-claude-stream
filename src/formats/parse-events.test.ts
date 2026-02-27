c576c0bc497366c77761cb5f616ba17b0dfc92aa
Ben Christel <ben.christel@gmail.com>
2026-02-27 15:39:57 -0800

    Add test for parseEvents

---
+++ src/formats/parse-events.test.ts
@@ -0,0 +1,19 @@
+import {describe, expect, it} from "@jest/globals";
+import {readdirSync} from "fs";
+import {join} from "path";
+
+const testcases = readdirSync(join(import.meta.dirname, "testcases"));
+
+function parseEvents(_: unknown) {
+    return [];
+}
+
+describe("parseEvents", () => {
+    it.each(testcases)("passes the test for %s", async (filename) => {
+        const {data, expected} = await import(
+            join(import.meta.dirname, "testcases", filename)
+        );
+
+        expect(parseEvents(data)).toEqual(expected);
+    });
+});
