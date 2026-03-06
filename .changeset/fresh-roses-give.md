a7ff63b20027f921d4ef5b87a006d5b958fade9e
Ben Christel <benchristel@khanacademy.org>
2026-03-06 11:33:17 -0800

    Compile CLI to JS rather than relying on @swc-node/register

---
+++ .changeset/fresh-roses-give.md
@@ -0,0 +1,5 @@
+---
+"@khanacademy/format-claude-stream": patch
+---
+
+Compile CLI to JavaScript instead of relying on @swc-node/register to compile it on the fly.
---
+++ src/cli/main.ts
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+import * as readline from "readline";
+import {ClaudeStreamFormatter} from "../core/claude-stream-formatter.ts";
+import {StandardOutput} from "../adapters/standard-output.ts";
+import {ChalkColorizer} from "../adapters/chalk-colorizer.ts";
+
+const pf = new ClaudeStreamFormatter(
+    new StandardOutput(),
+    new ChalkColorizer(),
+);
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    terminal: false,
+});
+
+let promiseChain = Promise.resolve();
+
+rl.on("line", (line) => {
+    promiseChain = promiseChain.then(async () => {
+        try {
+            await pf.write(JSON.parse(line));
+        } catch (e) {
+            console.error(e);
+            console.error("The bad line of input was:");
+            console.error(line);
+            process.exit(1);
+        }
+    });
+});
+
+rl.once("close", () => {});
